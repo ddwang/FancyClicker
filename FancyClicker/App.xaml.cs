550d9cec3e4877d73ed9e761cbd055df45c3e454
Wyatt Wang <wyattdecoder@gmail.com>
2022-01-17 21:40:13 -0800

    added OnStartup() mutex to ensure single instance

--- FancyClicker/App.xaml.cs
+++ FancyClicker/App.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 
@@ -11,7 +12,23 @@ namespace FancyClicker
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
+
     public partial class App : Application
     {
+        private static Mutex? _mutex = null;
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            bool createdNew;
+            _mutex = new Mutex(true, "FancyClicker_mutex", out createdNew);
+
+            if (!createdNew)
+            {
+                //app is already running! Exiting the application  
+                Application.Current.Shutdown();
+            }
+
+            base.OnStartup(e);
+        }
     }
 }
