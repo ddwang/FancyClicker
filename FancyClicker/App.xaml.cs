cb47185d2289a7fee98f25e8be62f2ed657a4b74
Peyton Wang <peytonprogrammer@gmail.com>
2022-01-19 13:50:48 -0800

    whitespaces

--- FancyClicker/App.xaml.cs
+++ FancyClicker/App.xaml.cs
@@ -1,34 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading;
-using System.Threading.Tasks;
+﻿using System.Threading;
 using System.Windows;
 
 namespace FancyClicker
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
+	/// <summary>
+	/// Interaction logic for App.xaml
+	/// </summary>
 
-    public partial class App : Application
-    {
-        private static Mutex? _mutex = null;
+	public partial class App : Application
+	{
+		private static Mutex? _mutex = null;
 
-        protected override void OnStartup(StartupEventArgs e)
-        {
-            bool createdNew;
-            _mutex = new Mutex(true, "FancyClicker_mutex", out createdNew);
+		protected override void OnStartup(StartupEventArgs e)
+		{
+			bool createdNew;
+			_mutex = new Mutex(true, "FancyClicker_mutex", out createdNew);
 
-            if (!createdNew)
-            {
-                //app is already running! Exiting the application  
-                Application.Current.Shutdown();
-            }
+			if (!createdNew)
+			{
+				//app is already running! Exiting the application  
+				Application.Current.Shutdown();
+			}
 
-            base.OnStartup(e);
-        }
-    }
+			base.OnStartup(e);
+		}
+	}
 }
--- FancyClicker/MainWindow.xaml.cs
+++ FancyClicker/MainWindow.xaml.cs
@@ -13,92 +13,92 @@ namespace FancyClicker
 	/// Interaction logic for MainWindow.xaml
 	/// </summary>
 	public partial class MainWindow : Window
-    {
-        [DllImport("user32.dll", CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
-        public static extern void mouse_event(uint dwFlags, uint dx, uint dy, uint cButtons, uint dwExtraInfo);
-        //Mouse actions
-        private const int MOUSEEVENTF_LEFTDOWN = 0x02;
-        private const int MOUSEEVENTF_LEFTUP = 0x04;
-        private const int MOUSEEVENTF_RIGHTDOWN = 0x08;
-        private const int MOUSEEVENTF_RIGHTUP = 0x10;
+	{
+		[DllImport("user32.dll", CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
+		public static extern void mouse_event(uint dwFlags, uint dx, uint dy, uint cButtons, uint dwExtraInfo);
+		//Mouse actions
+		private const int MOUSEEVENTF_LEFTDOWN = 0x02;
+		private const int MOUSEEVENTF_LEFTUP = 0x04;
+		private const int MOUSEEVENTF_RIGHTDOWN = 0x08;
+		private const int MOUSEEVENTF_RIGHTUP = 0x10;
 
-        #region Members
-        private ClickerStateModel _clickerStateModel;
-        private uint _lastMouseX, _lastMouseY;
+		#region Members
+		private ClickerStateModel _clickerStateModel;
+		private uint _lastMouseX, _lastMouseY;
 
-        private IKeyboardMouseEvents _globalHook;
-        private DispatcherTimer _dispatcherTimer;
-        #endregion
+		private IKeyboardMouseEvents _globalHook;
+		private DispatcherTimer _dispatcherTimer;
+		#endregion
 
 
 #pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
 
-        public MainWindow()
-        {
-            InitializeComponent();
+		public MainWindow()
+		{
+			InitializeComponent();
 
-            _globalHook = Hook.GlobalEvents();
-            _globalHook.MouseDownExt += GlobalHookMouseDownExt;
-            _globalHook.MouseUpExt += GlobalHook_MouseUpExt;
+			_globalHook = Hook.GlobalEvents();
+			_globalHook.MouseDownExt += GlobalHookMouseDownExt;
+			_globalHook.MouseUpExt += GlobalHook_MouseUpExt;
 
-            _dispatcherTimer = new DispatcherTimer();
-            _dispatcherTimer.Interval = TimeSpan.FromMilliseconds(25);  // (1000ms / 25ms = 40 cps)
-            _dispatcherTimer.Tick += DispatcherTimer_Tick;
+			_dispatcherTimer = new DispatcherTimer();
+			_dispatcherTimer.Interval = TimeSpan.FromMilliseconds(25);  // (1000ms / 25ms = 40 cps)
+			_dispatcherTimer.Tick += DispatcherTimer_Tick;
 
-            _clickerStateModel = (ClickerStateModel?)DataContext;
+			_clickerStateModel = (ClickerStateModel?)DataContext;
 
-            Closing += MainWindow_Closing;
-        }
+			Closing += MainWindow_Closing;
+		}
 
-        private void MainWindow_Closing(object sender, System.ComponentModel.CancelEventArgs e)
-        {
-            Unsubscribe();
-        }
+		private void MainWindow_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+		{
+			Unsubscribe();
+		}
 #pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
 
-        public void Unsubscribe()
-        {
-            _globalHook.MouseDownExt -= GlobalHookMouseDownExt;
-            _globalHook.MouseUpExt -= GlobalHook_MouseUpExt;
-            _dispatcherTimer.Tick -= DispatcherTimer_Tick;
+		public void Unsubscribe()
+		{
+			_globalHook.MouseDownExt -= GlobalHookMouseDownExt;
+			_globalHook.MouseUpExt -= GlobalHook_MouseUpExt;
+			_dispatcherTimer.Tick -= DispatcherTimer_Tick;
 
-            _globalHook.Dispose();
-        }
+			_globalHook.Dispose();
+		}
 
-        private void GlobalHookMouseDownExt(object sender, MouseEventExtArgs e)
-        {
-            if (e.Button == MouseButtons.Right
-                && (Keyboard.Modifiers & (ModifierKeys.Control | ModifierKeys.Windows)) > 0
-                && !_dispatcherTimer.IsEnabled)
-            {
-                Trace.WriteLine(String.Format("MouseDown: {0}; X={1}, Y={2}; Timestamp={3}", e.Button, e.X, e.Y, e.Timestamp));
-                _dispatcherTimer.Start();
+		private void GlobalHookMouseDownExt(object sender, MouseEventExtArgs e)
+		{
+			if (e.Button == MouseButtons.Right
+				&& (Keyboard.Modifiers & (ModifierKeys.Control | ModifierKeys.Windows)) > 0
+				&& !_dispatcherTimer.IsEnabled)
+			{
+				Trace.WriteLine(String.Format("MouseDown: {0}; X={1}, Y={2}; Timestamp={3}", e.Button, e.X, e.Y, e.Timestamp));
+				_dispatcherTimer.Start();
 
-                _lastMouseX = (uint)e.X;
-                _lastMouseY = (uint)e.Y;
+				_lastMouseX = (uint)e.X;
+				_lastMouseY = (uint)e.Y;
 
-                e.Handled = true;
-            }
-        }
+				e.Handled = true;
+			}
+		}
 
-        private void DispatcherTimer_Tick(object sender, EventArgs e)
-        {
-            Trace.WriteLine(String.Format("auto clicking at ({0}, {1})", _lastMouseX, _lastMouseY));
-            mouse_event(MOUSEEVENTF_LEFTDOWN | MOUSEEVENTF_LEFTUP, _lastMouseX, _lastMouseY, 0, 0);
+		private void DispatcherTimer_Tick(object sender, EventArgs e)
+		{
+			Trace.WriteLine(String.Format("auto clicking at ({0}, {1})", _lastMouseX, _lastMouseY));
+			mouse_event(MOUSEEVENTF_LEFTDOWN | MOUSEEVENTF_LEFTUP, _lastMouseX, _lastMouseY, 0, 0);
 
-            _clickerStateModel.incrementClickCount();
-        }
+			_clickerStateModel.incrementClickCount();
+		}
 
-        private void GlobalHook_MouseUpExt(object sender, MouseEventExtArgs e)
-        {
-            if (e.Button == MouseButtons.Right
-                && _dispatcherTimer.IsEnabled)
-            {
-                Trace.WriteLine(String.Format("MouseUp: {0}; X={1}, Y={2}", e.Button, e.X, e.Y, e.Timestamp));
-                _dispatcherTimer.Stop();
+		private void GlobalHook_MouseUpExt(object sender, MouseEventExtArgs e)
+		{
+			if (e.Button == MouseButtons.Right
+				&& _dispatcherTimer.IsEnabled)
+			{
+				Trace.WriteLine(String.Format("MouseUp: {0}; X={1}, Y={2}", e.Button, e.X, e.Y, e.Timestamp));
+				_dispatcherTimer.Stop();
 
-                e.Handled = true;
-            }
-        }
-    }
+				e.Handled = true;
+			}
+		}
+	}
 }
